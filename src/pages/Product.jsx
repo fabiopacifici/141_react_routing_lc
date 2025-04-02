9effaf30c655bed9f383badef94a61e0ffc3b8a6
fabiopacifici <boolean@fabiopacifici.com>
2025-04-02 10:53:51 +0200

    Add navigation button to return to the previous page in Product component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom" // 👈 import the useParams hook
+import { useParams } from "react-router-dom" // 👈 import the useParams hook 
+import { useNavigate } from 'react-router-dom' // 👈 Add the import for the useNavigate hook
 
 export default function Product() {
   const [product, setProduct] = useState(null)
+
   //console.log(product.description);
+  const navigate = useNavigate() // 👈 invoke the hook and save its result into a variable
   const { id } = useParams() // 👈 Use the object descruturing to extract the id segment
   console.log(id);
 
@@ -41,6 +44,12 @@ export default function Product() {
                         <img className="img-fluid" src={product.image} alt={product.title} />
                       </div>
                       <div className="col-12 col-md-7">
+
+                        {/* 👇 Add a button to navigate to the previous page (this is using the browser history) */}
+                        <button className="back" onClick={() => navigate(-1)}>
+                          <i className="bi bi-arrow-left"></i>
+                        </button>
+
                         <h1>Product: {product.title}</h1>
                         <p>{product.description}</p>
                         <div className="price fw-bold fs-2">${product.price}</div>
